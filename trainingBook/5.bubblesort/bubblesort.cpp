e5c3cba8229e67f6df14d44f7249febdf00289d3
jsggo2001 <jsggo2001@naver.com>
2024-09-02 13:18:47 +0900

    vector라이브러리를 사용한 솔트

--- trainingBook/5.bubblesort/bubblesort.cpp
+++ trainingBook/5.bubblesort/bubblesort.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-void bubblesort(int arr[], int n) {
+void bubbleSort(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j ++) {
             if (arr[j] > arr[j + 1]) {
@@ -14,7 +14,22 @@ void bubblesort(int arr[], int n) {
     }
 }
 
+vector<int> basicSortByLibrary(int arr[],int n) {
+
+    vector<int> vec(arr, arr + n);
+    cout << "벡터 솔트 정렬 : ";
+
+    sort(vec.begin(), vec.end());
+
+    for (int i : vec) {
+        cout << i << " "; 
+    }
+    cout << endl;
+    return vec;
+}
+
 void printArray(int arr[], int n) {
+    cout << "배열 버블정렬 : ";
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
@@ -24,10 +39,12 @@ void printArray(int arr[], int n) {
 int main() {
 
     int arr[] = {64,51,22,0,22,10,90};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    bubblesort(arr, n);
+    basicSortByLibrary(arr, n);
 
+    bubbleSort(arr, n);
+    
     printArray(arr, n);
 
     return 0;
